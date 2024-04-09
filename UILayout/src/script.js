41e024f1e6f99f5c17e419ee9c25f8b3ae98f443
mikocst <mikocastillo7@gmail.com>
2024-04-08 23:36:10 -0500

    created basic hero sectoin with nav bar

---
+++ UILayout/src/script.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = "flex";
+}
+
+function hideMenu() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = "none";
+}
