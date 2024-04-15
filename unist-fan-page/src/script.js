8c13c6f608142010ec76b8b1efcef71c33345e41
Miko Castillo <mikocastillo7@gmail.com>
2024-04-14 22:52:55 -0500

    created show sidebar function to reveal sidebar upon clicking the menu button

--- unist-fan-page/src/script.js
+++ unist-fan-page/src/script.js
@@ -1,1 +1,9 @@
-console.log('js works')
+function showSideBar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "flex"
+}
+
+function hideSideBar() {
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = "none"
+}
