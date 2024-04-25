f0da9b53a809630f7ffa1e9db8265987fea56bae
Miko Castillo <mikocastillo7@gmail.com>
2024-04-24 23:07:45 -0500

    added assets, created character select screen, and incorporated switch case for character button presses

--- unist-fan-page/src/script.js
+++ unist-fan-page/src/script.js
@@ -7,3 +7,20 @@ function hideSideBar() {
     const sidebar = document.querySelector(".sidebar")
     sidebar.style.display = "none"
 }
+
+let changeCharacter = (character) => {
+    {
+        switch(character) 
+        {
+            case "tsurugi":
+                document.getElementById("full").src = "../static/characters/tsurugi-full.svg";
+                document.getElementById("name").innerHTML = "Tsurugi";
+                break;  
+
+            case "kaguya":
+                document.getElementById("full").src = "../static/characters/kaguya-full.svg";
+                document.getElementById("name").innerHTML = "Kaguya";
+                break;
+        }
+    }
+}
